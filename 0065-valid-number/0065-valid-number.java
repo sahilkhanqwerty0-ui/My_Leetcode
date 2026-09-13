acf00b76a0b8d5c146d0e1692f762b3a07a68776
Sahil khan <sahilkhanqwerty0@gmail.com>
2026-09-13 23:54:05 +0530

    Time: 1 ms (100%), Space: 44 MB (51.83%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isNumber(String s) {
+
+        boolean digit = false;
+        boolean dot = false;
+        boolean exp = false;
+        boolean expDigit = true;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (c >= '0' && c <= '9') {
+                digit = true;
+
+                if (exp)
+                    expDigit = true;
+            }
+
+            else if (c == '+' || c == '-') {
+
+                if (i != 0 &&
+                    s.charAt(i - 1) != 'e' &&
+                    s.charAt(i - 1) != 'E')
+                    return false;
+
+            }
+
+            else if (c == '.') {
+
+                if (dot || exp)
+                    return false;
+
+                dot = true;
+            }
+
+            else if (c == 'e' || c == 'E') {
+
+                if (exp || !digit)
+                    return false;
+
+                exp = true;
+                expDigit = false;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        return digit && expDigit;
+    }
+}
