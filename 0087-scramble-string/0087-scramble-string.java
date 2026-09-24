5fbd4b9cc1850d7df57b12c2c77cbc686d346744
Sahil khan <sahilkhanqwerty0@gmail.com>
2026-09-24 23:48:53 +0530

    Time: 7 ms (68.88%), Space: 47 MB (39.56%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        if (s1.equals(s2)) return true;
+
+        String key = s1 + "#" + s2;
+        if (memo.containsKey(key))
+            return memo.get(key);
+
+        int n = s1.length();
+
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i) - 'a']++;
+            freq[s2.charAt(i) - 'a']--;
+        }
+
+        for (int x : freq) {
+            if (x != 0) {
+                memo.put(key, false);
+                return false;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            // No swap
+            if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
+                isScramble(s1.substring(i), s2.substring(i))) {
+                memo.put(key, true);
+                return true;
+            }
+
+            // Swap
+            if (isScramble(s1.substring(0, i), s2.substring(n - i)) &&
+                isScramble(s1.substring(i), s2.substring(0, n - i))) {
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+}
